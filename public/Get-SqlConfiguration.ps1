2aeb50e48f5bd02a35a9ff217e4f9e29ba028dd9
Marco Kleinert <marco.kleinert@gmail.com>
2016-08-31 17:55:03 +0200

    minor changes

--- public/Get-SqlConfiguration.ps1
+++ public/Get-SqlConfiguration.ps1
@@ -1,77 +1,81 @@
-#requires -Version 3
-<#
-        .SYNOPSIS
-        Get-SqlConfiguration
-        .DESCRIPTION
-        Get configuration for a SQL Server instance
-        .PARAMETER serverInstance
-        This is the name of the source instance. It's a mandatory parameter beause it is needed to retrieve the data.
-        .PARAMETER filter
-        The filter can be used the get only specified configuration values.
-        If no filter is set then all values are returned.	
-        .EXAMPLE
-        Get-Get-SqlConfiguration -serverInstance Server\Instance
-        .INPUTS
-        .OUTPUTS
-        Configuration Object
-        .NOTES
-        .LINK
-#>
-function Get-SqlConfiguration 
+﻿function Get-SQLConfiguration 
 {
-    param (
-        [Parameter(Mandatory = $true,ValueFromPipeline = $true)][string]$ServerInstance,
-        [Parameter(ValueFromPipeline = $true)][string[]]$filter
-    )
+  #requires -Version 3.0
+  <#
+      .SYNOPSIS
+      Get-SqlConfiguration
+      .DESCRIPTION
+      Get configuration for a SQL Server instance
+      .PARAMETER serverInstance
+      This is the name of the source instance. It's a mandatory parameter beause it is needed to retrieve the data.
+      .PARAMETER filter
+      The filter can be used the get only specified configuration values.
+      If no filter is set then all values are returned.	
+      .EXAMPLE
+      Get-Get-SqlConfiguration -serverInstance Server\Instance
+      .INPUTS
+      .OUTPUTS
+      Configuration Object
+      .NOTES
+      .LINK
+  #>
 
-    begin {
-        $null = [reflection.assembly]::LoadWithPartialName('Microsoft.SqlServer.Smo')
+  param (
+    [Parameter(Mandatory,ValueFromPipeline)][string]$ServerInstance,
+    [Parameter(ValueFromPipeline)][string[]]$filter = ''
+  )
 
-    }
-    process {
-        try 
-        {
-            Write-Verbose -Message 'Get SQL Server configuration...'
-            $server = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Server -ArgumentList $ServerInstance
+  begin {
+    $null = [reflection.assembly]::LoadWithPartialName('Microsoft.SqlServer.Smo')
+
+  }
+  process {
+    try 
+    {
+      Write-Verbose -Message 'Get SQL Server configuration...'
+      $server = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Server -ArgumentList $ServerInstance
                     
-            if($filter)
-            {
-                $configs = $server.Configuration |
-                Get-Member -MemberType Properties |
-                Where-Object -FilterScript {
-                    $filter.Contains($_.Name)
-                }
-            }
-            else
-            {
-                $configs = $server.Configuration |
-                Get-Member -MemberType Properties |
-                Where-Object -FilterScript {
-                    $_.Name -ne 'Properties'
-                }
-            }
+      if($filter)
+      {
+        $configs = $server.Configuration |
+        Get-Member -MemberType Properties |
+        Where-Object -FilterScript {
+          $filter.Contains($_.Name)
+        }
+      }
+      else
+      {
+        $configs = $server.Configuration |
+        Get-Member -MemberType Properties |
+        Where-Object -FilterScript {
+          $_.Name -ne 'Properties'
+        }
+      }
 
-            $SystemConfiguration = 
-            foreach($config in $configs)
-            {
-                New-Object -TypeName PSObject -Property ([Ordered]@{
-                        'Name'      = $config.Name
-                        'RunValue'  = $server.Configuration.$($config.Name).RunValue
-                        'ConfigValue' = $server.Configuration.$($config.Name).ConfigValue
-                        'IsDynamic' = $server.Configuration.$($config.Name).IsDynamic
-                })
-            }
-            return $SystemConfiguration
-        }
-        catch
-        {
-            Write-Error -Message $Error[0]
-            $err = $_.Exception
-            while ( $err.InnerException ) 
-            {
-                $err = $err.InnerException
-                Write-Output -InputObject $err.Message
-            }
-        }
+      $SystemConfiguration = 
+      foreach($config in $configs)
+      {
+        New-Object -TypeName PSObject -Property (@{
+            'Name'      = $config.Name
+            'RunValue'  = $server.Configuration.$($config.Name).RunValue
+            'ConfigValue' = $server.Configuration.$($config.Name).ConfigValue
+            'IsDynamic' = $server.Configuration.$($config.Name).IsDynamic
+        })
+      }
+      return $SystemConfiguration
     }
+    catch
+    {
+      Write-Error -Message $Error[0]
+      $err = $_.Exception
+      while ( $err.InnerException ) 
+      {
+        $err = $err.InnerException
+        Write-Output -InputObject $err.Message
+      }
+    }
+  }
+  end {
+    $server.ConnectionContext.Disconnect()
+  }
 }
