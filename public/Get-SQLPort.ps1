df97ad586377decc0c6d2e4a20f9ab44d0df5972
Marco Kleinert <marco.kleinert@gmail.com>
2016-08-31 19:31:22 +0200

    fixed minor issues more reliable now

--- public/Get-SQLPort.ps1
+++ public/Get-SQLPort.ps1
@@ -1,91 +1,96 @@
-﻿#requires -Version 3
-<#
-        .SYNOPSIS
-        Get-SqlPort
-        .DESCRIPTION
-        Retrieve SQL Server port configured for use using WMI
-        .PARAMETER serverInstance
-        This is the name of the source instance. It's a mandatory parameter beause it is needed to retrieve the data.
-        .EXAMPLE
-        Get-SqlPort -serverInstance Server\Instance
-        .INPUTS
-        .OUTPUTS
-        SQL Server Port #
-        .NOTES
-        Use server name only to target default instance
-        .LINK
-#> 
-function Get-SQLPort 
+﻿function Get-SQLPort 
 { 
-    [cmdletbinding()]
-    param([parameter(Mandatory)][string]$ServerInstance
-    )
+  #requires -Version 3.0
+  <#
+      .SYNOPSIS
+      Get-SqlPort
+      .DESCRIPTION
+      Retrieve SQL Server port configured for use using WMI
+      .PARAMETER serverInstance
+      This is the name of the source instance. It's a mandatory parameter beause it is needed to retrieve the data.
+      .EXAMPLE
+      Get-SQLPort -serverInstance Server\Instance
+      .INPUTS
+      .OUTPUTS
+      SQL Server Port # and type
+      .NOTES
+      Use server name only to target default instance
+      .LINK
+  #> 
 
-    begin {
-        $null = [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SMO')
-        
-        #Create a WMI namespace for SQL Server
-        $isSQLSupported = $false
-        if ($ver -eq 9) 
-        {
-            $WMInamespace = 'root\Microsoft\SqlServer\ComputerManagement'
-            $isSQLSupported = $true
+  [cmdletbinding()]
+  param([parameter(Mandatory)][string]$ServerInstance
+  )
+
+  begin {
+    $null = [Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SMO')
+  }
+  process {
+    try 
+    {
+      Write-Verbose -Message 'Retrieve SQL Server port configured for use using WMI...'
+            
+      $smoServer = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Server -ArgumentList $ServerInstance
+      $ver = $smoServer.Information.VersionMajor 
+      $serverName = $ServerInstance.Split('\')[0]
+      $instance = $ServerInstance.Split('\')[1]
+
+      if ($instance -eq $null) 
+      {
+        $instance = 'MSSQLSERVER'
+      } 
+
+      #Create a WMI namespace for SQL Server
+      $isSQLSupported = $false
+      if ($ver -eq 9) 
+      {
+        $WMInamespace = 'root\Microsoft\SqlServer\ComputerManagement'
+        $isSQLSupported = $true
+      }
+      else  
+      {
+        $WMInamespace = "root\Microsoft\SqlServer\ComputerManagement$ver"
+        $isSQLSupported = $true
+      } 
+
+      # Create a WMI query
+      $WQL = 'SELECT PropertyName, PropertyStrVal '
+      $WQL += 'FROM ServerNetworkProtocolProperty '
+      $WQL += "WHERE InstanceName = '" + $instance + "' AND "
+      $WQL += "IPAddressName = 'IPall' AND "
+      $WQL += "ProtocolName = 'Tcp'"
+
+      Write-Debug -Message $WQL
+
+      # Use PowerShell Get-WmiObject to run a WMI query 
+      if ($isSQLSupported) 
+      {
+        $output = Get-WmiObject -Query $WQL -ComputerName $serverName -Namespace $WMInamespace | 
+        Select-Object -Property PropertyName, @{
+          Name       = 'Port'
+          Expression = {
+            $_.PropertyStrVal
+          }
         }
-        else  
-        {
-            $WMInamespace = "root\Microsoft\SqlServer\ComputerManagement$ver"
-            $isSQLSupported = $true
-        } 
+      }
+      else 
+      {
+        $output = 'SQL Server version is unsupported'
+      }
+
+      $output
     }
-    process {
-        try 
-        {
-            Write-Verbose -Message 'Retrieve SQL Server port configured for use using WMI...'
-
-            $serverName = $ServerInstance.Split('\')[0]
-            $instance = $ServerInstance.Split('\')[1]
-
-            if ($instance -eq $null) 
-            {
-                $instance = 'MSSQLSERVER'
-            } 
-
-            $smoServer = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Server -ArgumentList $ServerInstance
-            $ver = $smoServer.Information.VersionMajor 
-
-            # Create a WMI query
-            $WQL = 'SELECT PropertyName, PropertyStrVal '
-            $WQL += 'FROM ServerNetworkProtocolProperty '
-            $WQL += "WHERE InstanceName = '" + $instance + "' AND "
-            $WQL += "IPAddressName = 'IPAll' AND "
-            $WQL += "ProtocolName = 'Tcp'"
-
-            Write-Debug -Message $WQL
-
-            # Use PowerShell Get-WmiObject to run a WMI query 
-            if ($isSQLSupported) 
-            {
-                $output = Get-WmiObject -Query $WQL -ComputerName $serverName -Namespace $WMInamespace | 
-                Select-Object -Property PropertyName, PropertyStrVal
-            }
-            else 
-            {
-                $output = 'SQL Server version is unsupported'
-            }
-
-            Write-Output -InputObject $output
-        }
-        catch
-        {
-            Write-Error -Message $Error[0]
-            $err = $_.Exception
-            while ( $err.InnerException ) 
-            {
-                $err = $err.InnerException
-                Write-Output -InputObject $err.Message
-            }
-        }
+    catch
+    {
+      Write-Error -Message $Error[0]
+      $err = $_.Exception
+      while ( $err.InnerException ) 
+      {
+        $err = $err.InnerException
+        Write-Output -InputObject $err.Message
+      }
     }
-    end { $message = "Getting port for `"$ServerInstance`"."
-    }
+  }
+  end { $server.ConnectionContext.Disconnect()
+  }
 }
