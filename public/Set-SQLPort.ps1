84e9383b9218c12c0f62588e730b55cf48da3f67
Marco Kleinert <marco.kleinert@hpe.com>
2016-09-02 13:32:02 +0200

    rewrite WMInamespace creation minor fixes

--- public/Set-SQLPort.ps1
+++ public/Set-SQLPort.ps1
@@ -17,58 +17,52 @@
 	.LINK
 #> 
 function Set-SQLPort { 
-  [cmdletbinding()]
-  param(
-    [Parameter(Mandatory=$true,ValueFromPipeline=$true)][string]$ServerInstance,
-    [Parameter(Mandatory=$true,ValueFromPipeline=$true)][ValidateRange(1433,1450)][int]$port
-  )
-  Begin {
-    $serverName = $serverInstance.Split('\')[0]
-    $instance = $serverInstance.Split('\')[1]
+    [cmdletbinding()]
+    param(
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)][string]$ServerInstance,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)][ValidateRange(1433,1450)][int]$port
+    )
+    Begin {
+        $serverName = $serverInstance.Split('\')[0]
+        $instance = $serverInstance.Split('\')[1]
 
-    if ($instance -eq $null) {
-      $instance = 'MSSQLSERVER'
-    }
+        if ($instance -eq $null) {
+            $instance = 'MSSQLSERVER'
+        }
 	
-    $null = [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SMO')
-    $Server = New-Object Microsoft.SqlServer.Management.Smo.Server $serverInstance
-    $ver = $Server.Information.VersionMajor
+        $null = [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SMO')
+        $Server = New-Object Microsoft.SqlServer.Management.Smo.Server $serverInstance
+        $ver = $Server.Information.VersionMajor
      
-    # Create a WMI namespace for SQL Server
-    $isSQLSupported = $false
-    if ($ver -eq 9) {
-      $WMInamespace = 'root\Microsoft\SqlServer\ComputerManagement'
-      $isSQLSupported = $true
-    } elseif ($ver -eq 10) {
-      $WMInamespace = 'root\Microsoft\SqlServer\ComputerManagement10'
-      $isSQLSupported = $true
-    } elseif ($ver -eq 11) {
-      $WMInamespace = 'root\Microsoft\SqlServer\ComputerManagement11'
-      $isSQLSupported = $true 
-    } elseif ($ver -eq 12) {
-      $WMInamespace = 'root\Microsoft\SqlServer\ComputerManagement12'
-      $isSQLSupported = $true 
-    } elseif ($ver -eq 13) {
-      $WMInamespace = 'root\Microsoft\SqlServer\ComputerManagement13'
-      $isSQLSupported = $true 
-    }  
-  }
-  Process { 
-    try {
-      $newport=Get-WmiObject -Namespace $WMInamespace -class ServerNetworkProtocolProperty -filter "PropertyName='TcpPort' and IPAddressName='IPAll' and Instancename='$Instance'" 
-      $dynport=Get-WmiObject -Namespace $WMInamespace -class ServerNetworkProtocolProperty -filter "PropertyName='TcpDynamicPorts' and IPAddressName='IPAll' and Instancename='$Instance'" 
-      $newPort.SetStringValue($port) | Out-Null
-      $dynport.SetStringValue('') | Out-Null
-      Write-verbose "Port changed to $Port - Service has to be restarted"
-    }
-    catch{
-      Write-Error $Error[0]
-      $err = $_.Exception
-      while ( $err.InnerException ) {
-        $err = $err.InnerException
-        Write-Output $err.Message
+      #Create a WMI namespace for SQL Server
+      $isSQLSupported = $false
+      if ($ver -eq 9) 
+      {
+        $WMInamespace = 'root\Microsoft\SqlServer\ComputerManagement'
+        $isSQLSupported = $true
       }
+      else  
+      {
+        $WMInamespace = "root\Microsoft\SqlServer\ComputerManagement$ver"
+        $isSQLSupported = $true
+      } 
+    
+    Process { 
+        try {
+            $newport=Get-WmiObject -Namespace $WMInamespace -class ServerNetworkProtocolProperty -filter "PropertyName='TcpPort' and IPAddressName='IPAll' and Instancename='$Instance'" 
+            $dynport=Get-WmiObject -Namespace $WMInamespace -class ServerNetworkProtocolProperty -filter "PropertyName='TcpDynamicPorts' and IPAddressName='IPAll' and Instancename='$Instance'" 
+            $newPort.SetStringValue($port) | Out-Null
+            $dynport.SetStringValue('') | Out-Null
+            Write-verbose "Port changed to $Port - Service has to be restarted"
+        }
+        catch{
+            Write-Error $Error[0]
+            $err = $_.Exception
+            while ( $err.InnerException ) {
+                $err = $err.InnerException
+                Write-Output $err.Message
+            }
+        }
     }
-  }
-  end { $message = "TCP port set to `"$port`" on `"$ServerInstance`".";  }
+    end { Write-verbos -message "TCP port set to `"$port`" on `"$ServerInstance`"."  }
 }
