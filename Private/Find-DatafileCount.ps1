d41324e107eeba0e8b819d7990ded080ffc22eed
Marco Kleinert <marco.kleinert@gmail.com>
2016-09-02 18:31:19 +0200

    new ruleset-based validation feature

---
+++ Private/Find-DatafileCount.ps1
@@ -0,0 +1,28 @@
+﻿function Find-DatafileCount 
+{ 
+  param (
+    [Parameter(Mandatory,ValueFromPipeline)][string]$ServerInstance
+  )
+  $null = [Reflection.Assembly]::LoadWithPartialName('Microsoft.SqlServer.SMO')
+  #create initial SMO object
+  $server = New-Object -TypeName ('Microsoft.SqlServer.Management.Smo.Server') -ArgumentList $ServerInstance
+     
+  $databases = $server.Databases
+
+  foreach($database in $databases)
+  {
+    # Get the filegroups for the database
+    $filegroups = $database.FileGroups
+
+    # Loop through all the filegroups
+    foreach($filegroup in $filegroups)
+    {
+      # Get all the data files from the filegroup
+      $i = ($filegroup.Files | Measure-Object).count
+    }
+    New-Object -TypeName PSObject -Property (@{
+        'database' = $database.Name
+        'filecount' = $i          
+    })
+  }
+}
