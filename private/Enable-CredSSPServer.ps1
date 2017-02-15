ae4a80f269db8cf9d32b759608502c3c80b9b06e
Marco Kleinert <marco.kleinert@hpe.com>
2017-02-15 12:07:45 +0100

    new CredSSP cmdlets

---
+++ private/Enable-CredSSPServer.ps1
@@ -0,0 +1,58 @@
+﻿#requires -Version 2.0
+function Enable-CredSSPServer
+{
+    <#
+            .SYNOPSIS
+            Enables and configures CredSSP Authentication to be used in PowerShell remoting sessions
+
+            .DESCRIPTION
+            Enabling CredSSP allows a caller from one remote session to authenticate on other remote 
+            resources. This is known as credential delegation. By default, PowerShell sessions do not 
+            use credSSP and therefore cannot bake a "second hop" to use other remote resources that 
+            require their authentication token.
+
+            Enable-CredSSPServer allows a computer to act as a delegate for another.
+           
+            .PARAMETER Computername
+            A ComputerName to enable the CredSSP server role.
+            If Computername is empty localhost will be used. 
+
+            .OUTPUTS
+            
+            .EXAMPLE
+            Enable-CredSSP srv1
+
+    #>
+    [CmdletBinding()]
+    param(
+        [string] $Computername = $env:COMPUTERNAME
+    )
+
+    if($Computername)
+    {
+        $FQDN = [Net.Dns]::GetHostByName(($Computername)) | Format-List -Property HostName | Out-String | ForEach-Object{ '{0}' -f $_.Split(':')[1].Trim() }
+    }
+
+    Write-Verbose -Message 'Configuring CredSSP settings...'
+    $credssp = Get-WSManCredSSP
+
+    $result = $credssp[1].IndexOf('is not ')
+
+    if($result -gt -1)
+    {
+        try 
+        {
+            Write-Verbose -Message "Enabling CredSSP server role for $FQDN" -Verbose
+            $null = Enable-WSManCredSSP -Role server -Force
+        }
+        catch 
+        {
+            Write-Error -Message "Enable-WSManCredSSP failed with: $_" -Verbose
+            $false
+        }
+    }
+    else
+    {
+        Write-Verbose -Message "CredSSP server role for $FQDN is already enabled" -Verbose
+    }
+}
---
+++ private/Test-PSRemoting.ps1
@@ -0,0 +1,74 @@
+﻿#requires -version 3.0
+ 
+Function Test-PSRemoting 
+{
+    <#
+        .SYNOPSIS
+        Purpose of "Test-PSRemoting" is to test WinRM.
+
+        .DESCRIPTION
+        The function submits an identification request that determines whether the WinRM service is running on a local or remote computer. 
+        If the tested computer is running the service, the cmdlet displays the WS-Management identity schema, the protocol version, the product vendor, and the product version of the tested service. 
+
+        .PARAMETER Computername
+        Describe parameter -Computername.
+
+        .PARAMETER Authentication
+        Describe parameter -Authentication.
+
+        .PARAMETER Credential
+        Describe parameter -Credential.
+
+        .EXAMPLE
+        Test-PSRemoting -Computername Value -Authentication Value -Credential Value
+        Describe what this call does
+
+        .NOTES
+        Place additional notes here.
+
+        .LINK
+        URLs to related sites
+        The first link is opened by Get-Help -Online Test-PSRemoting
+
+        .INPUTS
+        List of input types that are accepted by this function.
+
+        .OUTPUTS
+        List of output types produced by this function.
+    #>
+
+
+    [cmdletbinding()]
+ 
+    Param(
+        [Parameter(Position = 0,Mandatory,HelpMessage = 'Enter a computername',ValueFromPipeline)]
+        [ValidateNotNullorEmpty()]
+        [string]$Computername,
+        [Parameter(Position = 1)][string]$Authentication = 'default',    
+        [System.Management.Automation.Credential()]$Credential = [pscredential]::Empty
+
+    )
+ 
+    Begin {
+        Write-Verbose -Message "Starting $($MyInvocation.Mycommand)"  
+    } 
+ 
+    Process {
+        Write-Verbose -Message "Testing $Computername"
+        Try 
+        {
+            Test-WSMan -ComputerName $Computername -Credential $Credential -Authentication $Authentication -ErrorAction Stop
+            $True 
+        }
+        Catch 
+        {
+            Write-Error -Message $_.Exception.Message
+            $False
+        }
+ 
+    } 
+ 
+    End {
+        Write-Verbose -Message "Ending $($MyInvocation.Mycommand)"
+    } 
+}
